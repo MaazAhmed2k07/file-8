e4b34133bd00233fb45bf37d04e5179a9d2eb0c8
maaz ahmed <maazahmed0599@gmail.com>
2024-12-03 23:09:43 -0800

    half work done

---
+++ chap-1.js
@@ -0,0 +1,65 @@
+//CHAP # 1 ANSWER # 1 :
+
+alert("CHAP # 1 ANSWER # 1 : \n" + "Welcome To Our Website!!");
+console.log("CHAP # 1 ANSWER # 1 : \n" + "Welcome To Our Website!!");
+
+//CHAP # 1  ANSWER # 2 :
+
+alert("CHAP # 1 ANSWER # 2 : \n" + "Error ! Please Enter A Valid Code");
+console.log("CHAP # 1 ANSWER # 2 : \n" + "Error ! Please Enter A Valid Code")
+
+//CHAP # 1  ANSWER # 3 :
+
+alert("CHAP # 1  ANSWER # 3 : \nWelcome To JS Land ! \nHappy Coding !" );
+console.log("CHAP # 1  ANSWER # 3 : \nWelcome To JS Land ! \nHappy Coding !" )
+
+//CHAP # 2  ANSWER # 1 to 4 :
+
+var age = "17 years old"
+var myName = "Maaz Ahmed"
+var message = "Studying In 1st Year"
+
+alert("CHAP # 2  ANSWER # 1 to 4: \n" +myName);
+alert(age);
+alert(message);
+
+console.log("CHAP # 2  ANSWER # 1 to 4: \n" ,myName,"\n" ,age,"\n", message)
+
+//CHAP # 2  ANSWER # 5 :
+
+alert("CHAP # 2  ANSWER # 5: \n PIZZA \n PIZZ \n PIZ \n PI \n P");
+console.log("CHAP # 2  ANSWER # 5: \n PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+//CHAP # 2  ANSWER # 6 :
+
+var email =" abc.123@gmail.com";
+
+alert("CHAP # 2  ANSWER # 6: \n" +"My Gmail Is :" + email);
+console.log("CHAP # 2  ANSWER # 6: \n" +"My Gmail Is :" + email)
+
+//CHAP # 2  ANSWER # 7 :
+
+var book = "A Smarter Way To Learn Java Script !";
+
+alert("CHAP # 2  ANSWER # 7: \n"+"I Am Trying To Learn From The Book : " +book);
+console.log("CHAP # 2  ANSWER # 7: \n"+"I Am Trying To Learn From The Book : " +book)
+
+//CHAP # 3  ANSWER # 1 :
+
+var age1 = "I Am 17 Years Old";
+
+alert("CHAP # 3  ANSWER # 1: \n"+ age1);
+console.log("CHAP # 3  ANSWER # 1: \n"+ age1)
+
+//CHAP # 3  ANSWER # 3 :
+
+var birthYear = "2007"
+console.log("CHAP # 3  ANSWER # 3: \n"+"My Birth Year Is : "+ birthYear," \nData Type Of My Declared Variable Is Number" );
+
+//CHAP # 3  ANSWER # 4 :
+
+var visitorName = "John Doe"
+var productTitle = "ABC General Store"
+var quantity = "3 Kg Wheat"
+
+console.log("CHAP # 3  ANSWER # 4: \n"+visitorName+" ordered "+ quantity+" from "+ productTitle);
